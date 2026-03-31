7a4ba29fdc5007f699f26fd1005212dbc3003b84
adrichey <1534554+adrichey@users.noreply.github.com>
2026-03-31 10:07:47 -0500

    Refactor the router into a class

--- public/index.php
+++ public/index.php
@@ -1,7 +1,13 @@
 <?php
 
 require '../helpers.php';
+require basePath('router.php');
 
+$router = new Router();
+
+$routes = require basePath('routes.php');
+
+$method = $_SERVER['REQUEST_METHOD'];
 $uri = $_SERVER['REQUEST_URI'];
 
-require basePath('router.php');
+$router->route($method, $uri);
--- router.php
+++ router.php
@@ -1,10 +1,53 @@
 <?php
 
-$routes = require basePath('routes.php');
+class Router {
+    protected $routes = [];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    http_response_code(404);
-    require(basePath($routes['404']));
+    private function registerRoute(string $method, string $uri, string $controller): void
+    {
+        array_push($this->routes, [
+            'method' => $method,
+            'uri' => $uri,
+            'controller' => $controller,
+        ]);
+    }
+
+    public function get(string $uri, string $controller): void
+    {
+        $this->registerRoute('GET', $uri, $controller);
+    }
+
+    public function post(string $uri, string $controller): void
+    {
+        $this->registerRoute('POST', $uri, $controller);
+    }
+
+    public function put(string $uri, string $controller): void
+    {
+        $this->registerRoute('PUT', $uri, $controller);
+    }
+
+    public function delete(string $uri, string $controller): void
+    {
+        $this->registerRoute('DELETE', $uri, $controller);
+    }
+
+    public function route(string $method, string $uri): void
+    {
+        foreach ($this->routes as $route) {
+            if ($route['method'] == $method && $route['uri'] == $uri) {
+                require(basePath($route['controller']));
+                return;
+            }
+        }
+
+        $this->error();
+    }
+
+    private function error(int $httpCode = 404): void
+    {
+        http_response_code($httpCode);
+        loadView("error/{$httpCode}");
+        exit;
+    }
 }
