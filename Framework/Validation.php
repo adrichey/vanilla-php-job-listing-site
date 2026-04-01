b20758110fb92b85ec6971d6483215f990eec78c
adrichey <1534554+adrichey@users.noreply.github.com>
2026-04-01 12:17:15 -0500

    Create validation helper class

---
+++ Framework/Validation.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Framework;
+
+class Validation {
+    public static function string(string $value, int $min = 1, int $max = INF): bool {
+        if (!is_string($value)) {
+            return false;
+        }
+
+        $length = strlen($value);
+
+        if ($length < $min) {
+            return false;
+        }
+
+        if ($length > $max) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static function email(string $value): bool {
+        if (filter_var(trim($value), FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public static function match(string $value1, string $value2): bool {
+        if (trim($value1) === trim($value2)) {
+            return true;
+        }
+
+        return false;
+    }
+}
