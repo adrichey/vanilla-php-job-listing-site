319540d145272ea38e052e6419ec0c6fd8d9f5f6
adrichey <1534554+adrichey@users.noreply.github.com>
2026-04-03 15:05:30 -0500

    Add DB config directory to the repo

---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'host' => '', // Ex: 'localhost'
+    'port' => '', // Ex: 3306
+    'dbname' => '', // Ex: vanilla_jobs
+    'username' => '', // Ex: vanilla_jobs_user
+    'password' => '', // Ex: password123
+];
